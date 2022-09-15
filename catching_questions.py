383be420e8603075a8b6043a35df267ca2dae65e
Farideh Moteeyan <faridehmoteeyannajar20@gmail.com>
2022-09-16 00:01:03 +0430

    ver.2

--- catching_questions.py
+++ catching_questions.py
@@ -1,10 +1,23 @@
 import os
+import argparse
 
-#making dir(main_files) links
+# to test parsel in pycharm use configuration(up page_next to the run)
+parser = argparse.ArgumentParser(prog="readme.task",description="""put this program in your folder
+ we extraction question(part witch start with# in markdown filles)from dirs
+ make a complite question with their link in readme.file""")
+parser.add_argument("github_name",help='your username of github')
+parser.add_argument("repo_name",help='project name(repositpry name) in github')
+args = parser.parse_args()
+
+def adress_maker(github_name,repo_name):
+    add = f"https://github.com/{github_name}/{repo_name}"
+    return add
+
+
 def link_title (x,add):
    return f'# [{x}]({add}/blob/main/{x}/Readme.md)'
 
-# get question in each dir and link qustion to ther places in dir
+
 def get_question_from_file(x,add):
     return_str = ""
     with open(f'{x}\\Readme.md', encoding="utf-8") as readme_file:
@@ -16,27 +29,20 @@ def get_question_from_file(x,add):
                 return_str = return_str + f'[{clean_line}]({add}/blob/main/{x}#{spilit_clean_line})'+os.linesep
     return return_str
 
-#due to we need just one arguman in write
+
 def joiner (add):
-   links =""
-   for title in files:
-      links += link_title(title,add) +"\n" + get_question_from_file(title,add)
-      #return links
-   return links
+    files = [f for f in os.listdir() if os.path.isdir(f)]
+    files = [f for f in files if not '.' in f]
+    links =""
+    for title in files:
+        links += link_title(title,add) +"\n" + get_question_from_file(title,add)
+    return links
 
-print("Instructions".center(100,"-"))
-print("""put this program in your folder
- we extraction question(part witch start with# in markdown filles)from dirs
- make a complite question with their link in readme.file""")
+def main(github_name, repo_name):
+    with open("Readme.md", "w+") as main_file:
+        main_file.write(joiner(adress_maker(github_name, repo_name)))
+        main_file.seek(0)
+        print(main_file.read())
 
-add = input("enter your github repository address")
-
-files_names = os.listdir()
-files = [f for f in os.listdir()  if  os.path.isdir(f)]
-files = [f for f in files if not '.' in f]
-
-#make a folder and put links here
-with open("Readme.md","w+") as main_file:
-    main_file.write(joiner(add))
-    main_file.seek(0)
-    print(main_file.read())
+if __name__ == '__main__':
+    main(args.github_name,args.repo_name)
